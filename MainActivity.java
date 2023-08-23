9f5bc5964451fe8bde85bf1b983d32f072517a9d
ye_zi <15721002894@163.com>
2023-08-23 22:34:22 +0800

    1

---
+++ MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.myapplicationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreat(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
